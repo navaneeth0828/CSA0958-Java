a473c436426026e288ab612a11579742ecfdcd56
navaneeth0828 <navaneeth0828@gmail.com>
2024-12-17 08:57:56 +0530

    Add files via upload

---
+++ variableExample.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        boolean result = true;
+        char capitalC = 'C';
+        byte b = 100;
+        short s = 10000;
+        int i = 100000;
+        long creditCardNumber = 1234_5678_9012_3456L;
+        System.out.println("Boolean result: " + result);
+        System.out.println("Character capitalC: " + capitalC);
+        System.out.println("Byte b: " + b);
+        System.out.println("Short s: " + s);
+        System.out.println("Integer i: " + i);
+        System.out.println("Long creditCardNumber: " + creditCardNumber);
+    }
+}
